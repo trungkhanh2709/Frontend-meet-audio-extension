7203d2d94993c14f93c1d248c27f325b6c302c97
trungkhanh2709 <111423000+trungkhanh2709@users.noreply.github.com>
2025-07-23 00:38:20 +0700

    fix cors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import AgentForm from "./components/AgentForm";
-import SearchHistory from "./components/AgentWithHistory"; // bạn sẽ tạo file này
 
 function App() {
   return (
     <div className="flex h-screen">
       
 
-      {/* Form nhập Agent bên phải */}
     
         <AgentForm />
     </div>
